a1cff003dd733e86ea591fb18acab439fe395dad
Naxetee <naxetee.a.r@uma.es>
2022-05-14 09:22:34 +0200

    Header Terminado

--- app/src/App.js
+++ app/src/App.js
@@ -1,8 +1,7 @@
 import Header from "./Componentes/Header";
 import Footer from "./Componentes/Footer";
 import Feed from "./Componentes/Feed";
-import "./style/App.css";
-
+import "./style/App.css"
 function App() {
     return (
         <div className="App">
--- app/src/Componentes/Header.js
+++ app/src/Componentes/Header.js
@@ -4,42 +4,52 @@ import "../style/Global.css";
 import Logo from "./Logo.js";
 
 export default function Header() {
-    const [clase, setClase] = useState("noDisplay");
-    return (
-        <>
-            <div className="Header">
-                <div className="TitleHeader">
-                    <Logo></Logo>
-                    <h2 className="Title">PadelNow</h2>
-                </div>
-                <div className="Navigate">
-                    <div className="Search">
-                        <img src="/lense.svg" alt="Search" />
-                        <input
-                            type="text"
-                            id="Search"
-                            placeholder="Buscar..."
-                        />
-                    </div>
-                    <button
-                        onMouseEnter={() => setClase("Desplegable")}
-                        onMouseLeave={() => setClase("noDisplay")}
-                    >
-                        <img
-                            className="Menu"
-                            src="menuButton.svg"
-                            alt="Botón Menu"
-                        ></img>
-                    </button>
-                </div>
-            </div>
-            <div
-                className={clase}
-                onMouseEnter={() => setClase("Desplegable")}
-                onMouseLeave={() => setClase("noDisplay")}
-            >
-                DESPLEGABLE
-            </div>
-        </>
-    );
+  const [clase, setClase] = useState("noDisplay");
+  return (
+    <>
+      <div className="Header">
+        <div className="TitleHeader">
+          <Logo></Logo>
+          <h2 className="Title">PadelNow</h2>
+        </div>
+        <div className="Navigate">
+          <div className="Search">
+            <img src="/lense.svg" alt="Search" />
+            <input type="text" id="Search" placeholder="Buscar..." />
+          </div>
+          <button
+            onMouseEnter={() => setClase("Desplegable")}
+            onMouseLeave={() => setClase("noDisplay")}
+            onClick={() => {
+              if (clase === "Desplegable") {
+                setClase("noDisplay");
+              } else {
+                setClase("Desplegable");
+              }
+            }}
+          >
+            <img className="Menu" src="menuButton.svg" alt="Botón Menu"></img>
+          </button>
+        </div>
+      </div>
+      <div
+        className={clase}
+        onMouseEnter={() => setClase("Desplegable")}
+        onMouseLeave={() => setClase("noDisplay")}
+      >
+        <div className="DesplegableTop">
+          <button className="DesplegableTitles">Partidos</button>
+          <button className="DesplegableTitles">Parejas</button>
+          <button className="DesplegableTitles">Circuitos</button>
+          <button className="DesplegableTitles">Torneos</button>
+        </div>
+          <div className="SepHor"></div>
+        <div className="DesplegableBottom">
+          <button className="DesplegableTitles">Modo Claro</button>
+          <button className="DesplegableTitles">Sobre Nosotros</button>
+          <button className="DesplegableTitles">Enviar Incidencia</button>
+        </div>
+      </div>
+    </>
+  );
 }
