1d08019014910bd4058368da8292fe6e77339ba6
mdmatii <0610705247@uma.es>
2022-05-16 10:41:01 +0200

    Jugador

--- app/src/App.js
+++ app/src/App.js
@@ -2,11 +2,13 @@ import Header from "./Componentes/Header";
 import Footer from "./Componentes/Footer";
 import Feed from "./Componentes/Feed";
 import "./style/App.css"
+import Jugador from "./Componentes/Jugador";
 function App() {
     return (
         <div className="App">
             <Header></Header>
             <Feed></Feed>
+            <Jugador></Jugador>
             <Footer></Footer>
         </div>
     );
