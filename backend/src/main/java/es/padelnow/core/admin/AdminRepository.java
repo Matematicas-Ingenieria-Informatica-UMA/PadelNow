9baae806108906dfca85d8c175f71ff45c85c734
Naxetee <naxetee.a.r@uma.es>
2022-05-20 12:32:18 +0200

    Co-authored-by: Arturo Aguilera González <arturoaguileraa@users.noreply.github.com> Co-authored-by: Matiird <Matiird@users.noreply.github.com> Co-authored-by: Mario Quiñones Madrona <jann9@users.noreply.github.com>

--- backend/src/main/java/es/padelnow/core/admin/AdminRepository.java
+++
@@ -1,7 +0,0 @@
-package es.padelnow.core.admin;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AdminRepository extends JpaRepository<Admin, Long> {
-
-}
--- backend/src/main/java/es/padelnow/core/partido/useCases/PartidoCreator.java
+++
@@ -1,4 +0,0 @@
-package es.padelnow.core.partido.useCases;
-
-public class PartidoCreator {
-}
