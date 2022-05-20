eb45de5e3b9adedf74838bba1c5af803336e6293
Naxetee <naxetee.a.r@uma.es>
2022-05-20 22:09:26 +0200

    JugadorCRUD

--- app/src/admin/Entidades/Jugador/JugadorCRUD.js
+++ app/src/admin/Entidades/Jugador/JugadorCRUD.js
@@ -1,78 +1,91 @@
 import React from "react";
 import { useState } from "react";
+import { Link } from "wouter";
 
 import "./Jugadores.css";
 
 export default function JugadorCRUD() {
-    const [clase, setClase] = useState("JugadorNoDisplay");
-    const [boton, setBoton] = useState("JugadorVerMas JugadorFont");
+  const [clase, setClase] = useState("JugadorNoDisplay");
+  const [eliminar, setEliminar] = useState("Jugador");
+  const [boton, setBoton] = useState("JugadorVerMas JugadorFont");
 
-    return (
-        <>
-            <div className="Jugador">
-                <img
-                    src="https://www.worldpadeltour.com/media-content/2022/05/alejandra-salazar-bengoechea-c50aaf0cc5-220x260.JPG"
-                    className="Imagen"
-                    alt="ProfilePhoto"
-                />
-                <div>
-                    <div className="JugadorSepHor"></div>
-                    <h1 className="Nombre">ALEJANDRA SALAZAR BENGOECHEA</h1>
-                    <div className="JugadorSepHor"></div>
-                    <div className={clase}>
-                        <div className="keyValue">
-                            <h4>Nacimiento</h4>
-                            <h3>31/12/1985</h3>
-                        </div>
-                        <div className="keyValue">
-                            <h4>Nacionalidad</h4>
-                            <h3>Española</h3>
-                        </div>
-                        <div className="keyValue">
-                            <h4>Ciudad</h4>
-                            <h3>Madrid</h3>
-                        </div>
-                        <div className="keyValue">
-                            <h4>Ranking</h4>
-                            <h3>1</h3>
-                        </div>
-                        <div className="keyValue">
-                            <h4>Brazo Dominante</h4>
-                            <h3>Diestra</h3>
-                        </div>
-                        <div className="keyValue">
-                            <h4>Compañerx</h4>
-                            <h3>Gemma Triay</h3>
-                        </div>
-                        <div className="keyValue">
-                            <h4>Altura</h4>
-                            <h3>1'68</h3>
-                        </div>
-                        <div className="keyValue">
-                            <h4>Posición</h4>
-                            <h3>Revés</h3>
-                        </div>
-                        <button
-                            className="JugadorVerMas JugadorFont"
-                            onClick={() => {
-                                setClase("JugadorNoDisplay");
-                                setBoton("JugadorVerMas JugadorFont");
-                            }}
-                        >
-                            Ver Menos
-                        </button>
-                    </div>
-                    <button
-                        className={boton}
-                        onClick={() => {
-                            setClase("JugadorFont");
-                            setBoton("JugadorNoDisplay");
-                        }}
-                    >
-                        Ver Más
-                    </button>
-                </div>
+  return (
+    <>
+      <div className={eliminar}>
+        <div className="CRUDElements">
+          <img
+            onClick={() => setEliminar("CRUDNoDisplay")}
+            src="/Delete.svg"
+            alt="Delete"
+            className="ButtonPlayer"
+          />
+          <Link to="/admin/recursos/jugadores/crear">
+            <img src="/Edit.svg" alt="Delete" className="ButtonPlayer" />
+          </Link>
+        </div>
+        <img
+          src="https://www.worldpadeltour.com/media-content/2022/05/alejandra-salazar-bengoechea-c50aaf0cc5-220x260.JPG"
+          className="Imagen"
+          alt="ProfilePhoto"
+        />
+        <div>
+          <div className="JugadorSepHor"></div>
+          <h1 className="Nombre">ALEJANDRA SALAZAR BENGOECHEA</h1>
+          <div className="JugadorSepHor"></div>
+          <div className={clase}>
+            <div className="keyValue">
+              <h4>Nacimiento</h4>
+              <h3>31/12/1985</h3>
             </div>
-        </>
-    );
+            <div className="keyValue">
+              <h4>Nacionalidad</h4>
+              <h3>Española</h3>
+            </div>
+            <div className="keyValue">
+              <h4>Ciudad</h4>
+              <h3>Madrid</h3>
+            </div>
+            <div className="keyValue">
+              <h4>Ranking</h4>
+              <h3>1</h3>
+            </div>
+            <div className="keyValue">
+              <h4>Brazo Dominante</h4>
+              <h3>Diestra</h3>
+            </div>
+            <div className="keyValue">
+              <h4>Compañerx</h4>
+              <h3>Gemma Triay</h3>
+            </div>
+            <div className="keyValue">
+              <h4>Altura</h4>
+              <h3>1'68</h3>
+            </div>
+            <div className="keyValue">
+              <h4>Posición</h4>
+              <h3>Revés</h3>
+            </div>
+            <button
+              className="JugadorVerMas JugadorFont"
+              onClick={() => {
+                setClase("JugadorNoDisplay");
+                setBoton("JugadorVerMas JugadorFont");
+              }}
+            >
+              Ver Menos
+            </button>
+          </div>
+          <button
+            className={boton}
+            onClick={() => {
+              setClase("JugadorFont");
+              setBoton("JugadorNoDisplay");
+            }}
+          >
+            Ver Más
+          </button>
+        </div>
+      </div>
+    </>
+  );
 }
