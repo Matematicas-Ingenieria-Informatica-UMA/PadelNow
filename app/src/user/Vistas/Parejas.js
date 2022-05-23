215b0f8cb074562ff80d5fcad44a1890a39075fb
Naxetee <naxetee.a.r@uma.es>
2022-05-23 09:51:53 +0200

    Generos responsive

--- app/src/user/Vistas/Parejas.js
+++ app/src/user/Vistas/Parejas.js
@@ -7,26 +7,50 @@ import { parejas } from "../../BBDD/ParejasBBDD.js";
 import "../style/Parejas.css";
 
 export default function Parejas() {
-  return (
-    <>
-      <Generos />
-
-      <div className="Parejas">
-        <div className="ParejasGenero">
-          {parejas.map((pareja) => {
-            if (pareja.Genero === "Masculino") {
-              return <Pareja ID={pareja.id} />;
-            }
-          })}
-        </div>
-        <div className="ParejasGenero">
-          {parejas.map((pareja) => {
-            if (pareja.Genero === "Femenino") {
-              return <Pareja ID={pareja.id} />;
-            }
-          })}
-        </div>
-      </div>
-    </>
-  );
+  let x = window.matchMedia("(min-width:700px");
+  function mediaQueri(x) {
+    if (x.matches) {
+      return (
+        <>
+          <Generos />
+          <div className="Parejas">
+            <div className="ParejasGenero">
+              {parejas.map((pareja) => {
+                if (pareja.Genero === "Masculino") {
+                  return <Pareja ID={pareja.id} />;
+                }
+              })}
+            </div>
+            <div className="ParejasGenero">
+              {parejas.map((pareja) => {
+                if (pareja.Genero === "Femenino") {
+                  return <Pareja ID={pareja.id} />;
+                }
+              })}
+            </div>
+          </div>
+        </>
+      );
+    } else {
+      return (
+        <>
+          <div className="generoButton">
+            <img src="/Masc.svg" className="generoNoDisplay" />
+            <img src="/Fem.svg" className="generoNoDisplay" />
+          </div>
+          <div className="Parejas">
+            <div className="ParejasGenero">
+              {parejas.map((pareja) => {
+                if (pareja.Genero === "Masculino") {
+                  return <Pareja ID={pareja.id} />;
+                }
+              })}
+            </div>
+          </div>
+        </>
+      );
+    }
+  }
+  return mediaQueri(x);
+  x.addEventListener(mediaQueri);
 }
