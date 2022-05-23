c4aa0d209fc6e8d3567d0ffd2c22edbc8b3ff351
Naxetee <naxetee.a.r@uma.es>
2022-05-23 12:18:05 +0200

    Parejas Responsive v2

--- app/src/user/Vistas/Parejas.js
+++ app/src/user/Vistas/Parejas.js
@@ -1,56 +1,44 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Pareja from "../Componentes/Pareja.js";
 import Generos from "../Componentes/Generos.js";
 
 import { parejas } from "../../BBDD/ParejasBBDD.js";
 
 import "../style/Parejas.css";
-
 export default function Parejas() {
-  let x = window.matchMedia("(min-width:700px");
-  function mediaQueri(x) {
-    if (x.matches) {
-      return (
-        <>
-          <Generos />
-          <div className="Parejas">
-            <div className="ParejasGenero">
-              {parejas.map((pareja) => {
-                if (pareja.Genero === "Masculino") {
-                  return <Pareja ID={pareja.id} />;
-                }
-              })}
-            </div>
-            <div className="ParejasGenero">
-              {parejas.map((pareja) => {
-                if (pareja.Genero === "Femenino") {
-                  return <Pareja ID={pareja.id} />;
-                }
-              })}
-            </div>
+  const [width, setWidth] = useState(0);
+
+  useEffect(() => {
+    const updateWidth = () => {
+      const x = document.body.clientWidth;
+      setWidth(x);
+    };
+    updateWidth();
+    window.addEventListener("resize", updateWidth);
+  }, [width]);
+  if (width < 700) {
+    return <h1>CREAR UNA NUEVA VISTA PARA RESPONSIVE</h1>;
+  } else {
+    return (
+      <>
+        <Generos />
+        <div className="Parejas">
+          <div className="ParejasGenero">
+            {parejas.map((pareja) => {
+              if (pareja.Genero === "Masculino") {
+                return <Pareja ID={pareja.id} />;
+              }
+            })}
           </div>
-        </>
-      );
-    } else {
-      return (
-        <>
-          <div className="generoButton">
-            <img src="/Masc.svg" className="generoNoDisplay" />
-            <img src="/Fem.svg" className="generoNoDisplay" />
+          <div className="ParejasGenero">
+            {parejas.map((pareja) => {
+              if (pareja.Genero === "Femenino") {
+                return <Pareja ID={pareja.id} />;
+              }
+            })}
           </div>
-          <div className="Parejas">
-            <div className="ParejasGenero">
-              {parejas.map((pareja) => {
-                if (pareja.Genero === "Masculino") {
-                  return <Pareja ID={pareja.id} />;
-                }
-              })}
-            </div>
-          </div>
-        </>
-      );
-    }
+        </div>
+      </>
+    );
   }
-  return mediaQueri(x);
-  x.addEventListener(mediaQueri);
 }
