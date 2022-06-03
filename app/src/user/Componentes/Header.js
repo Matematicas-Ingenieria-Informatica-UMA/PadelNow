1c37aceeeda406a06b7245e13317e2945734b7a0
Naxetee <naxetee.a.r@uma.es>
2022-06-03 12:20:04 +0200

    Desplegable Search

--- app/src/user/Componentes/Header.js
+++ app/src/user/Componentes/Header.js
@@ -1,5 +1,5 @@
-import React, {useState} from "react";
-import {Link} from "react-router-dom";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 
 import "../style/Header.css";
 import "../style/Global.css";
@@ -7,75 +7,89 @@ import Logo from "./Logo.js";
 // import Jugadores from "../Vistas/Jugadores.js";
 
 export default function Header() {
-    const [clase, setClase] = useState("noDisplay");
-    return (
-        <>
-            <div className="Header">
-                <Link className="TitleHeader" to="/">
-                    <Logo></Logo>
-                    <h2 className="Title">PadelNow</h2>
-                </Link>
-                <div className="Navigate">
-                    <div className="Search">
-                        <Link to="/admin">
-                            <img src="/lense.svg" alt="Search"/>
-                        </Link>
-                        <input
-                            type="text"
-                            id="Search"
-                            placeholder="Buscar..."
-                            className="SearchInput"
-                        />
-                    </div>
-                    <button
-                        onMouseEnter={() => setClase("Desplegable")}
-                        onMouseLeave={() => setClase("noDisplay")}
-                        onClick={() => {
-                            if (clase === "Desplegable") {
-                                setClase("noDisplay");
-                            } else {
-                                setClase("Desplegable");
-                            }
-                        }}
-                    >
-                        <img
-                            className="Menu"
-                            src="menuButton.svg"
-                            alt="Botón Menu"
-                        ></img>
-                    </button>
-                </div>
+  const [clase, setClase] = useState("noDisplay");
+  const [search, setSearch] = useState("");
+  const [claseSearch, setClaseSearch] = useState("noDisplay");
+  return (
+    <>
+      <div className="Header">
+        <Link className="TitleHeader" to="/">
+          <Logo></Logo>
+          <h2 className="Title">PadelNow</h2>
+        </Link>
+        <div className="Navigate">
+          <div className="SearchWithDesplegable">
+            <div className="Search">
+              <Link to="/admin">
+                <img src="/lense.svg" alt="Search" />
+              </Link>
+              <input
+                type="text"
+                id="Search"
+                placeholder="Buscar..."
+                className="SearchInput"
+                value={search}
+                onChange={(e) => {
+                  if (e.target.value.length >= 3) {
+                    setClaseSearch("DesplegableSearch");
+                  } else {
+                    setClaseSearch("noDisplay");
+                  }
+                  setSearch(e.target.value);
+                }}
+              />
             </div>
-            <div
-                className={clase}
-                onMouseEnter={() => setClase("Desplegable")}
-                onMouseLeave={() => setClase("noDisplay")}
-            >
-                <div className="DesplegableTop">
-                    <Link to="/partidos" className="DesplegableTitles">
-                        Últimos Partidos
-                    </Link>
-                    <Link to="/jugadores" className="DesplegableTitles">
-                        Ver Jugadores
-                    </Link>
-                    <Link to="/parejas" className="DesplegableTitles">
-                        Ver Parejas
-                    </Link>
-                    <Link to="/torneos" className="DesplegableTitles">
-                        Ver Torneos
-                    </Link>
-                    <Link to="/noticias" className="DesplegableTitles">
-                        Todas las Noticias
-                    </Link>
-                </div>
-                <div className="HeaderSepHor"></div>
-                <div className="DesplegableBottom">
-                    <button className="DesplegableTitles">Modo Claro</button>
-                    <Link to="/enviarincidencia" className="DesplegableTitles">
-                        Enviar Incidencia
-                    </Link>
-                </div>
+            <div className={claseSearch}>
+              <h4>hola</h4>
+              <h4>hola</h4>
+              <h4>hola</h4>
             </div>
-        </>
-    );
+          </div>
+          <button
+            onMouseEnter={() => setClase("Desplegable")}
+            onMouseLeave={() => setClase("noDisplay")}
+            onClick={() => {
+              if (clase === "Desplegable") {
+                setClase("noDisplay");
+              } else {
+                setClase("Desplegable");
+              }
+            }}
+          >
+            <img className="Menu" src="menuButton.svg" alt="Botón Menu"></img>
+          </button>
+        </div>
+      </div>
+      <div
+        className={clase}
+        onMouseEnter={() => setClase("Desplegable")}
+        onMouseLeave={() => setClase("noDisplay")}
+      >
+        <div className="DesplegableTop">
+          <Link to="/partidos" className="DesplegableTitles">
+            Últimos Partidos
+          </Link>
+          <Link to="/jugadores" className="DesplegableTitles">
+            Ver Jugadores
+          </Link>
+          <Link to="/parejas" className="DesplegableTitles">
+            Ver Parejas
+          </Link>
+          <Link to="/torneos" className="DesplegableTitles">
+            Ver Torneos
+          </Link>
+          <Link to="/noticias" className="DesplegableTitles">
+            Todas las Noticias
+          </Link>
+        </div>
+        <div className="HeaderSepHor"></div>
+        <div className="DesplegableBottom">
+          <button className="DesplegableTitles">Modo Claro</button>
+          <Link to="/enviarincidencia" className="DesplegableTitles">
+            Enviar Incidencia
+          </Link>
+        </div>
+      </div>
+    </>
+  );
 }
