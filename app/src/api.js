97a90ecdc88f423859054ba859203057b8d62e01
Alejandro Rey Leyva <alereyleyva@gmail.com>
2022-06-05 23:57:33 +0200

    API URL

--- app/src/api.js
+++ app/src/api.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:8080",
+  baseURL: "https://padel-now.herokuapp.com",
+  //baseURL: "https://localhost:8080",
 });
 
 export default api;
